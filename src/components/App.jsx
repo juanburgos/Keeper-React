e85e365f7e81f4273c46c5888f6eadf587b8b75d
JuanBurgos <JuanBurgos@outlook.com>
2021-03-12 10:36:58 -0500

    Add & style Note

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from "./Header";
+import Note from "./Note";
 import Footer from "./Footer";
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
 
         <div>
             <Header />
+                <Note />
             <Footer />
         </div>
 
